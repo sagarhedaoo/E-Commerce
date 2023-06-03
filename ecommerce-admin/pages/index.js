8f3a40d9b3231dffe300e3453788ae980feb2c92
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-02 23:31:39 -0400

    Create Components and Products page
    Did Customizations in admin panel. Added Icons using heroicons svg and a <nav> to display all

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,26 +1,11 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <>
-      Signed in as {session.user.email} <br />
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
+    <Layout>
+      <div className="text-blue-900">Hello, {session.user.email}</div>
+    </Layout>
   );
 }
