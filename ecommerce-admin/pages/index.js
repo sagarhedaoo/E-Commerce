26bc4e51e6d64247a88cae3efe683fc0a69493e7
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-03 00:07:31 -0400

    Created Dashboard basic UI with username and image

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -3,9 +3,19 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+  console.log({ session });
+  if (!session) return;
   return (
     <Layout>
-      <div className="text-blue-900">Hello, {session.user.email}</div>
+      <div className="text-blue-900  flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300  gap-1 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="" className="w-10 h-10" />
+          <span className="py-2 px-2">{session?.user?.name}</span>
+        </div>
+      </div>
     </Layout>
   );
 }
