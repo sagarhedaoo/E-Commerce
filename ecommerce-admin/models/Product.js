9727c1306ae9692df63cb33ad0c23583aebcdc9a
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-03 03:24:27 -0400

    Added products.js and made several changes to the code

---
+++ ecommerce-admin/models/Product.js
@@ -0,0 +1,11 @@
+import { Schema } from "mongoose";
+import { model } from "mongoose";
+import { models } from "mongoose";
+
+const ProductSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+});
+
+export const Product = models.Product || model("Product", ProductSchema);
---
+++ ecommerce-admin/pages/api/products.js
@@ -0,0 +1,24 @@
+import clientPromise from "@/lib/mongodb";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import mongoose, { mongo } from "mongoose";
+
+export default async function handle(req, res) {
+  const { method } = req;
+
+  await mongooseConnect();
+
+  if (method === "GET") {
+    res.json(await Product.find());
+  }
+
+  if (method === "POST") {
+    const { title, description, price } = req.body;
+    const productDoc = await Product.create({
+      title,
+      description,
+      price,
+    });
+    res.json(productDoc);
+  }
+}
