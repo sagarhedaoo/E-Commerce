1b88426633182429a18a412e030ef0687c501761
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-03 00:37:55 -0400

    Created Add Products Page

--- ecommerce-admin/pages/products.js
+++ ecommerce-admin/pages/products.js
@@ -1,5 +1,15 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
-  return <Layout>products page here</Layout>;
+  return (
+    <Layout>
+      <Link
+        className="bg-blue-900 text-white rounded-md py-1 px-2"
+        href={"/products/new"}
+      >
+        Add New Product
+      </Link>
+    </Layout>
+  );
 }
