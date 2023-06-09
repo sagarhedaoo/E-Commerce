34fd90c2f62ce857925a418179a3988fe68e5a06
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-09 15:58:28 -0400

    Fixed Cart Numbers when adding new product

---
+++ ecommerce-front/components/CartContext.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext({});
+
+export function CartContextProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState([]);
+  function addProduct(productId) {
+    setCartProducts((prev) => [...prev, productId]);
+  }
+  return (
+    <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
