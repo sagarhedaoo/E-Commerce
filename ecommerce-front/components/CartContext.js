4eba7fe5dd3120be34a680268b30d4f402b71305
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-09 17:16:03 -0400

    Cart Page UI changes
    Added Functionality to the cart page by adding and removing quantity of the product and calcualting the total price

--- ecommerce-front/components/CartContext.js
+++ ecommerce-front/components/CartContext.js
@@ -1,14 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({});
 
 export function CartContextProvider({ children }) {
+  const ls = typeof window !== "undefined" ? window.localStorage : null;
   const [cartProducts, setCartProducts] = useState([]);
+  useEffect(() => {
+    if (cartProducts?.length > 0) {
+      ls?.setItem("cart", JSON.stringify(cartProducts));
+    }
+  }, [cartProducts]);
+  useEffect(() => {
+    if (ls && ls.getItem("cart")) {
+      setCartProducts(JSON.parse(ls.getItem("cart")));
+    }
+  }, []);
   function addProduct(productId) {
     setCartProducts((prev) => [...prev, productId]);
   }
+  function removeProduct(productId) {
+    setCartProducts((prev) => {
+      const pos = prev.indexOf(productId);
+      if (pos !== -1) {
+        return prev.filter((value, index) => index !== pos);
+      }
+      return prev;
+    });
+  }
   return (
-    <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct }}>
+    <CartContext.Provider
+      value={{ cartProducts, setCartProducts, addProduct, removeProduct }}
+    >
       {children}
     </CartContext.Provider>
   );
