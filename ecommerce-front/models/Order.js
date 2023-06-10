607f9da111e7960384cff0271357b5779106ef17
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-10 08:15:41 -0400

    Added Stripe Integration
    Stripe functionality completed with developer strip keys

---
+++ ecommerce-front/models/Order.js
@@ -0,0 +1,14 @@
+import { model, models, Schema } from "mongoose";
+
+const OrderSchema = new Schema({
+  line_items: Object,
+  name: String,
+  city: String,
+  email: String,
+  postalCode: String,
+  streetAddress: String,
+  country: String,
+  paid: Boolean,
+});
+
+export const Order = models?.Order || model("Order", OrderSchema);
--- ecommerce-front/pages/cart.js
+++ ecommerce-front/pages/cart.js
@@ -82,6 +82,37 @@ export default function CartPage() {
     total += price;
   }
 
+  async function goToPayment() {
+    const response = await axios.post("/api/checkout", {
+      name,
+      email,
+      city,
+      postalCode,
+      streetAddress,
+      country,
+      cartProducts,
+    });
+    if (response.data.url) {
+      window.location = response.data.url;
+    }
+  }
+
+  if (window.location.href.includes("success")) {
+    return (
+      <>
+        <Header />
+        <Center>
+          <ColumnsWrapper>
+            <Box>
+              <h1>Thank you for your purchase!</h1>
+              <p>We will email you when your order will be processed</p>
+            </Box>
+          </ColumnsWrapper>
+        </Center>
+      </>
+    );
+  }
+
   return (
     <>
       <Header />
@@ -150,12 +181,14 @@ export default function CartPage() {
                 type="text"
                 placeholder="Name"
                 value={name}
+                name="name"
                 onChange={(ev) => setName(ev.target.value)}
               />
               <Input
                 type="text"
                 placeholder="Email"
                 value={email}
+                name="email"
                 onChange={(ev) => setEmail(ev.target.value)}
               />
               <CityHolder>
@@ -163,12 +196,14 @@ export default function CartPage() {
                   type="text"
                   placeholder="City"
                   value={city}
+                  name="city"
                   onChange={(ev) => setCity(ev.target.value)}
                 />
                 <Input
                   type="text"
                   placeholder="Postal Code"
                   value={postalCode}
+                  name="postalCode"
                   onChange={(ev) => setPostalCode(ev.target.value)}
                 />
               </CityHolder>
@@ -176,16 +211,19 @@ export default function CartPage() {
                 type="text"
                 placeholder="Street Address"
                 value={streetAddress}
+                name="streetAddress"
                 onChange={(ev) => setStreetAddress(ev.target.value)}
               />
               <Input
                 type="text"
                 placeholder="Country"
                 value={country}
+                name="country"
                 onChange={(ev) => setCountry(ev.target.value)}
               />
-              <Button black block y>
-                COntinue to Payment
+
+              <Button black block onClick={goToPayment}>
+                Continue to Payment
               </Button>
             </Box>
           )}
