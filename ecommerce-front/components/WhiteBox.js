a6cb948298cba378020e65ea98f3d6ce15927278
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-11 08:58:07 -0400

    Added Single Product pages
    Images Working, Add to cart and pricing bug issues

---
+++ ecommerce-front/components/WhiteBox.js
@@ -0,0 +1,9 @@
+import { Styled, styled } from "styled-components";
+
+const WhiteBox = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+`;
+
+export default WhiteBox;
