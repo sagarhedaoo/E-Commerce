cd502d6220c82ad1a2570a97e89af22f17927c5c
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-11 09:06:20 -0400

    Added Price and Add to Cart Functionality

--- ecommerce-front/pages/product/[id].js
+++ ecommerce-front/pages/product/[id].js
@@ -1,10 +1,14 @@
+import Button from "@/components/Button";
+import { CartContext } from "@/components/CartContext";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import ProductImages from "@/components/ProductImages";
 import Title from "@/components/Title";
 import WhiteBox from "@/components/WhiteBox";
+import CartIcon from "@/components/icons/CartIcon";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import { useContext } from "react";
 import { styled } from "styled-components";
 
 const ColWrapper = styled.div`
@@ -14,7 +18,17 @@ const ColWrapper = styled.div`
   margin-top: 40px;
 `;
 
+const PriceRow = styled.div`
+  gap: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Price = styled.span`
+  font-size: 1.5rem;
+`;
+
 export default function ProductPage({ product }) {
+  const { addProduct } = useContext(CartContext);
   return (
     <>
       <Header />
@@ -27,6 +41,16 @@ export default function ProductPage({ product }) {
           <div>
             <Title>{product.title}</Title>
             <p>{product.description}</p>
+            <PriceRow>
+              <div>
+                <Price> ${product.price}</Price>
+              </div>
+              <div>
+                <Button primary onClick={() => addProduct(product._id)}>
+                  <CartIcon /> Add to Cart
+                </Button>
+              </div>
+            </PriceRow>
           </div>
         </ColWrapper>
       </Center>
