16c95eb50dd53f17fa4f5be93e15fb3811f7503c
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-12 09:29:03 -0400

    Categories page fixed
    Filters yet to add

--- ecommerce-front/pages/categories.js
+++ ecommerce-front/pages/categories.js
@@ -1,18 +1,102 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
-import ProductsGrid from "@/components/ProductsGrid";
+import ProductBox from "@/components/ProductBox";
 import Title from "@/components/Title";
-import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
+import { styled } from "styled-components";
+import Link from "next/link";
 
-export default function CategoryPage({}) {
+const CategoryGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+const CategoryTitle = styled.div`
+  margin-top: 10px;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  h2 {
+    margin-bottom: 10px;
+    margin-top: 10px;
+  }
+  a {
+    color: #555;
+    display: inline-flex;
+  }
+`;
+
+const CategoryWrapper = styled.div`
+  margin-bottom: 40px;
+`;
+
+const ShowAllSquare = styled(Link)`
+  background-color: #ddd;
+  border-radius: 10px;
+  height: 160px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  color: #555;
+  text-decoration: none;
+`;
+
+export default function CategoriesPage({ mainCategories, categoriesProducts }) {
   return (
     <>
       <Header />
       <Center>
-        <Title>Category Page</Title>
-        {/* <ProductsGrid products={products} /> */}
+        {mainCategories.map((cat) => (
+          <CategoryWrapper>
+            <CategoryTitle>
+              <h2>{cat.name}</h2>
+              <div>
+                <Link href={"/category/" + cat._id}>Show All {cat.name}s</Link>
+              </div>
+            </CategoryTitle>
+            <CategoryGrid>
+              {categoriesProducts[cat._id].map((p) => (
+                <ProductBox {...p} />
+              ))}
+              <ShowAllSquare href={"/category/" + cat._id}>
+                Show All &rarr;
+              </ShowAllSquare>
+            </CategoryGrid>
+          </CategoryWrapper>
+        ))}
       </Center>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  await mongooseConnect();
+  const categories = await Category.find();
+  const mainCategories = categories.filter((c) => !c.parent);
+  const categoriesProducts = {};
+  for (const mainCat of mainCategories) {
+    const mainCatId = mainCat._id.toString();
+    const childCatIds = categories
+      .filter((c) => c?.parent?.toString() === mainCatId)
+      .map((c) => c._id.toString());
+    const categoriesIds = [mainCatId, ...childCatIds];
+    const products = await Product.find({ category: categoriesIds }, null, {
+      limit: 3,
+      sort: { _id: -1 },
+    });
+    categoriesProducts[mainCatId] = products;
+  }
+  return {
+    props: {
+      mainCategories: JSON.parse(JSON.stringify(mainCategories)),
+      categoriesProducts: JSON.parse(JSON.stringify(categoriesProducts)),
+    },
+  };
 }
---
+++ ecommerce-front/pages/category/[id].js
@@ -0,0 +1,33 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import ProductsGrid from "@/components/ProductsGrid";
+import Title from "@/components/Title";
+import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
+import ProductBox from "@/components/ProductBox";
+
+export default function CategoryPage({ category, products }) {
+  return (
+    <>
+      <Header />
+      <Center>
+        <Title>{category.name}</Title>
+        <ProductsGrid products={products} />
+      </Center>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const category = await Category.findById(context.query.id);
+  const subCategories = await Category.find({ parent: category._id });
+  const catIds = [category._id, ...subCategories.map((c) => c._id)];
+  const products = await Product.find({ category: catIds });
+  console.log(products);
+  return {
+    props: {
+      category: JSON.parse(JSON.stringify(category)),
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+}
