02296922d95c1dc4d76634c73af693c59b4fb574
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-17 13:49:26 -0400

    Past Orders Added

--- ecommerce-front/models/Order.js
+++ ecommerce-front/models/Order.js
@@ -2,10 +2,11 @@ import { model, models, Schema } from "mongoose";
 
 const OrderSchema = new Schema(
   {
+    userEmail: String,
     line_items: Object,
     name: String,
-    city: String,
     email: String,
+    city: String,
     postalCode: String,
     streetAddress: String,
     country: String,
